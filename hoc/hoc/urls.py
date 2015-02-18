11ce4274b1fea4dd00756e9e1de5afe7d2718a12
flow <clapieflor@eisti.eu>
2015-02-18 21:00:15 +0100

    let's go to code => 2am

--- hoc/hoc/urls.py
+++ hoc/hoc/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.views.generic import TemplateView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hoc.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
 )
