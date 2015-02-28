c098f4d1fb044eccd4bffc15c4535d8f488ef4f2
flow <you@example.com>
2015-02-28 19:24:16 +0100

    big update need to fix the upgrade of materialize

--- hoc/hoc/urls.py
+++ hoc/hoc/urls.py
@@ -17,9 +17,10 @@ urlpatterns = patterns('',
 
     url(r'^poles$', TemplateView.as_view(template_name="poles.html"), name='poles'),
 
+    url(r'^voyages$', TemplateView.as_view(template_name="voyages.html"), name='voyages'),
+
 
     url(r'^challenges/$', ChallengesList.as_view(template_name="challenges.html"), name='challenges'),
-
     url(r'^challenge/$', ChallengeCreate.as_view(template_name="challenges_new.html"), name='challenge'),
 
     url(r'^thanks/$', 'index.views.thanks', name='thanks'),
